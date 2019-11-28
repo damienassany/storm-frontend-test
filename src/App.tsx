211805fdfa0d2129b80e5cc9d2fb7cb0d3680795
Damien Assany <damienassany@macbook-pro-de-damien.home>
2019-11-28 23:50:43 +0100

    chore(styled-components): setup library + example

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,81 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
+
+const Logo = styled.img`
+  height: 150px;
+`;
+
+const Header = styled.header`
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`;
+
+const Wrapper = styled.div`
+  text-align: center;
+`;
+
+const AppLink = styled.a`
+  color: #09d3ac;
+`;
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <Wrapper>
+      <Header>
+        <Logo src={logo} alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
+        <AppLink
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
-      </header>
-    </div>
+        </AppLink>
+      </Header>
+    </Wrapper>
   );
 }
 
 export default App;
+
+
+// import React from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+// import styled from 'styled-components';
+
+// const Logo = styled.img`
+//   height: 40vmin;
+// `;
+
+// const App: React.FC = () => {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.tsx</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
